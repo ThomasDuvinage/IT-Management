8cecea55e83d41a3e5ecddeeaf16e028307ce296
thomas.duvinage <thomas.duvinage@utbm.fr>
2021-05-31 13:04:03 +0200

    Create ScreenController.java

---
+++ src/src/guiController/ScreenController.java
@@ -0,0 +1,29 @@
+package guiController;
+
+import java.util.HashMap;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class ScreenController {
+	private HashMap<String, Scene> screenMap = new HashMap<>();
+	private Stage main;
+	
+	public ScreenController(Stage main) {
+		this.main = main;
+	}
+	
+	public void addScreen(String name, Scene scene) {
+		screenMap.put(name, scene);
+	}
+	
+	public void removeScreen(String name) {
+		screenMap.remove(name);
+	}
+	
+	public void activate(String name) {
+		screenMap.get(name).getStylesheets().add("application.css");
+		main.setScene(screenMap.get(name));
+	}
+}
