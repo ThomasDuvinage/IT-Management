59e7553a8a89e48fe39dda5dce271ee71ddb756e
thomas.duvinage <43036801+totordudu@users.noreply.github.com>
2021-06-01 00:11:53 +0200

    Update ConnexionController.java

--- src/src/guiController/ConnexionController.java
+++ src/src/guiController/ConnexionController.java
@@ -8,38 +8,40 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import model.UserDB;
 
-public class ConnexionController implements Initializable{
+public class ConnexionController implements Initializable {
 	@FXML
 	private TextField f_username;
 	@FXML
 	private PasswordField f_password;
 
+	private UserDB model_userDB;
+
+	public ConnexionController(UserDB userDB) {
+		this.model_userDB = userDB;
+	}
 
 	// Event Listener on Button[#b_login].onAction
 	@FXML
 	public void login() {
 		// TODO: check in the database that username + password match
-		if (f_username.getText().toString().equals("admin")) {
-			if (f_password.getText().toString().equals("my_password")) {
-				System.out.println("Credentials correct");
-			}
-			else {
-				System.out.println("Invalid username or password");
-			}
-		}
-		else {
-			System.out.println("Invalid username");
+		if (this.model_userDB.checkConnect(f_username.getText().toString(), f_password.getText().toString())) {
+			System.out.println("Credentials correct");
+		} else {
+			System.out.println("Invalid username or password");
 		}
 	}
+
 	// Event Listener on Button[#b_quit].onAction
 	@FXML
 	public void quit() {
 		System.exit(0);
 	}
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
