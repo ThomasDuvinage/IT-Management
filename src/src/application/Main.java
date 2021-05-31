28acca25aae16c1f2a01e59577824df180226890
thomas.duvinage <43036801+totordudu@users.noreply.github.com>
2021-06-01 00:11:55 +0200

    Update Main.java

--- src/src/application/Main.java
+++ src/src/application/Main.java
@@ -1,23 +1,29 @@
 package application;
-	
+
+import guiController.ConnexionController;
 import guiController.ScreenController;
 import javafx.application.Application;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
+import model.UserDB;
 import javafx.fxml.FXMLLoader;
 
-
 public class Main extends Application {
+	private ConnexionController connexion_controller;
+	private UserDB model_users;
 	
+	private ScreenController screenController;
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {			
-			ScreenController screenController = new ScreenController(primaryStage);
+			screenController = new ScreenController(primaryStage);
 			
-			screenController.addScreen("connexion", new Scene(FXMLLoader.load(getClass().getResource("connexion.fxml"))));
-			screenController.addScreen("main", new Scene(FXMLLoader.load(getClass().getResource("main.fxml"))));
+			initializeModels();
+			initializeControllers();
 			
-			screenController.activate("main");
+			setViews();
+			
+			screenController.activate("connexion");
 			
 			primaryStage.setTitle("Menu principal");
 			primaryStage.show();
@@ -26,6 +32,24 @@ public class Main extends Application {
 		}
 	}
 	
+	private void setViews() {
+		FXMLLoader connexion_file = new FXMLLoader(getClass().getResource("connexion.fxml"));
+		connexion_file.setController(connexion_controller);
+		screenController.addScreen("connexion", connexion_file);
+		
+		FXMLLoader main_view = new FXMLLoader(getClass().getResource("main.fxml"));
+		//TODO add controller 
+		screenController.addScreen("main", main_view);
+	}
+	
+	private void initializeModels() {
+		this.model_users = new UserDB();
+	}
+
+	private void initializeControllers() {
+		this.connexion_controller = new ConnexionController(this.model_users);
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
